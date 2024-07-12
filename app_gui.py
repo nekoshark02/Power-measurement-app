ed7f052e9ff09177d7dadf390bf3349037bb7169
nekoshark02 <kyle2020namm@zb.ztv.ne.jp>
2024-07-12 10:12:12 +0900

    tkinterでグラフの外形を表示させた

--- app_gui.py
+++ app_gui.py
@@ -1,19 +1,46 @@
 ################ ライブラリの導入 ####################
 import sys
 import tkinter as tk
-import matplotlib.pyplot as plt
+from tkinter import ttk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+import matplotlib.animation as animation
 import numpy as np
-###################### 変数 #########################
-windowWidth = 1000
-windowHeight = 800
-##############################################
-#class 
-################### 画面の設定 ######################
-window = tk.Tk() # 画面の表示
-window.title("電力測定グラフ") # 画面のタイトル
-window.geometry(f"{windowWidth}x{windowHeight}") # 画面の大きさ
-window.iconbitmap("./icon/analysis-analytics-graph.ico") # 画面のアイコン
 
+class GUIApplication(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        # タイトル表示
+        self.master.title("電力測定グラフ")
+        self.master.geometry()
+        # matplot配置用フレーム
+        frame = tk.Frame(self.master)
+        # matplotlibの描画領域の作成
+        fig = Figure()
+        self.ax = fig.add_subplot(1, 1, 1)
+        self.fig_canvas = FigureCanvasTkAgg(fig, frame)
+        self.toolbar = NavigationToolbar2Tk(self.fig_canvas, frame)
+        self.fig_canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+        # フレームをウィンドウに配置
+        frame.pack()
+    
+    def PowerGraph(self):
+        # 要素の空の配列をいったん置く
+        x = []
+        y = []
+        
+        
+        
+    def Quit(self):
+        self.quit()
+        self.destroy()
+        
 
-
-window.mainloop() # 画面表示命令
+def main():
+    root = tk.Tk()
+    app = GUIApplication(master=root)
+    app.mainloop()
+    
+if __name__ == "__main__":
+    main()
